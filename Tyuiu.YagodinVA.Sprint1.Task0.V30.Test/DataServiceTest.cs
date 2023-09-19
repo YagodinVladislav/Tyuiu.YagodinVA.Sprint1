704a7ac7cfc2d45951f24deed871b94b50f969e6
Tyuiu.Yagodinv@outlook.com <Tyuiu.Yagodinv@outlook.com>
2023-09-19 21:39:33 +0500

    Add project files.

---
+++ Tyuiu.YagodinVA.Sprint1.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.YagodinVA.Sprint1.Task0.V30.Lib;
+
+
+namespace Tyuiu.YagodinVA.Sprint1.Task0.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(96, res);
+        }
+    }
+}
