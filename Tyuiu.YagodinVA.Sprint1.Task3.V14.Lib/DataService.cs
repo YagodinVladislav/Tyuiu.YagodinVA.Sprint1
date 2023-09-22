489344cc4a00a1a92ed5fb11e7712456af1496d2
Tyuiu.Yagodinv@outlook.com <Tyuiu.Yagodinv@outlook.com>
2023-09-22 22:46:47 +0500

    Task3

---
+++ Tyuiu.YagodinVA.Sprint1.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.YagodinVA.Sprint1.Task3.V14.Lib
+{
+    public class DataService : ISprint1Task3V14
+    {
+        public double ReverseNumber(double number)
+        {
+            string s = number.ToString();
+            char[] ar = s.ToCharArray();
+            Array.Reverse(ar);
+            s = new String(ar);
+            number = Convert.ToDouble(s);
+            return number;
+        }
+    }
+}
---
+++ Tyuiu.YagodinVA.Sprint1.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.YagodinVA.Sprint1.Task3.V14.Lib;
+
+namespace Tyuiu.YagodinVA.Sprint1.Task3.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService dataService = new DataService();
+            double number = 13579, wait = 97531, res = dataService.ReverseNumber(number);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
